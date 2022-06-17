5d38c975ed2b529a092285e02a9a8b501ea07ceb
pavithramuddu93 <pavithramuddu93>
2022-06-17 15:46:11 +0530

    UC3 added

--- src/com/blz/ds/LinkedList.java
+++ src/com/blz/ds/LinkedList.java
@@ -1,5 +1,7 @@
 package com.blz.ds;
 
+import static sun.jvm.hotspot.runtime.BasicObjectLock.size;
+
 public class LinkedList {
 
     Node head;
@@ -14,24 +16,77 @@ public class LinkedList {
 
     }
 
-    public void addFirst(Object data)
-    {
+    public void addFirst(Object data) {
         Node newNode = new Node(data);
 
-        if(head == null)
-            head=newNode;
-        else
-        {
+        if (head == null)
+            head = newNode;
+        else {
             Node temp = head;
             head = newNode;
             head.next = temp;
 
         }
     }
+    public void addLast(Object data) {
+        Node newNode = new Node(data);
+
+        if (head == null)
+            head = newNode; // head=>[1 |null ] // list is notempty
+        else {
+            Node temp = head; // tempararory pointer
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+
+        }
+
+    }
 
 
+    public void insertAtSpecificIndex(int userIndex, Object data) {
 
+        // User trying to insert @ first position
+        if (userIndex == 0)
+            addFirst(data);
 
+            // // User trying to insert @ last position
+        else if (userIndex == size() - 1)
+            addLast(data);
+
+            // User trying to insert @ invalid position
+        else if (userIndex < 0 || userIndex >= size())
+            System.out.println("Invalid index");
+            // User trying to insert @ specific index
+        else {
+            // Creating New Node
+            Node newNode = new Node(data);
+
+            // To track traversing
+            int index = 0;
+
+            // Pointers to track left & right side elements
+            Node left = head;
+            Node right = left.next;
+
+            // Traverse or Move till last element before user entered index
+            while (index < userIndex - 1) {
+                left = left.next;
+                right = right.next;
+                index++;
+            }
+
+            // Connnecting new Node with right side elements
+            newNode.next = right;
+            // Connnecting left side elements with new Node
+            left.next = newNode;
+
+        }
+
+    }
 
     public void display() {
         /*
--- src/com/blz/ds/LinkedListRunner.java
+++ src/com/blz/ds/LinkedListRunner.java
@@ -2,15 +2,27 @@ package com.blz.ds;
 
 public class LinkedListRunner {
     public static void main(String[] args) {
+               // 10 ==>20==>30==>40==>50
+                LinkedList linkedList = new LinkedList();
 
-        LinkedList linkedList = new LinkedList();
+                linkedList.addLast(10);
+                linkedList.addLast(20);
+                linkedList.addLast(30);
+                linkedList.addLast(40);
+                linkedList.addLast(50);
+                linkedList.display();
 
-        linkedList.display();
-        linkedList.addFirst(70);
-        linkedList.addFirst(30);
-        linkedList.addFirst(56);
+                linkedList.insertAtSpecificIndex(2, 21);
+                linkedList.display();
 
-        linkedList.display();
+                linkedList.insertAtSpecificIndex(4, 35);
+                linkedList.display();
 
-    }
-}
+
+                linkedList.insertAtSpecificIndex(0, 5);
+                linkedList.display();
+
+
+            }
+
+        }
